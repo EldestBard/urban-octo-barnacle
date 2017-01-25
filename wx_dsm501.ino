e14a42380ca8eedabdd1fb1fb3e4d7b78c69f002
EldestBard <449734150@qq.com>
2017-01-25 16:04:57 +0800

    Update wx_dsm501.ino

--- wx_dsm501.ino
+++ wx_dsm501.ino
@@ -7,8 +7,8 @@ byte mac[] = {
   0x5E, 0x3A, 0x87, 0x57, 0xDD, 0x14};
 EthernetClient client;
 char server[] = "wxxcc.applinzi.com";
-unsigned long lastConnectionTime = 0;
-boolean lastConnected = false;
+unsigned long lastConnectionTime = 0;          
+boolean lastConnected = false;                 
 const unsigned long postingInterval = 200*1000;
 unsigned long duration;
 unsigned long starttime;
@@ -18,73 +18,57 @@ float ratio = 0;
 float concentration = 0;
 float pm25val = 0.05;
 const float pm25coef = 0.00207916725464941;
-//处理DSM501传回来的数据
-float dsm501(void)
-{
+void setup(){
+  Ethernet.begin(mac);
+  pinMode(pin,INPUT);
+  starttime = millis();
+}
+  
+void loop(void){
+  if(state == '0'){
+    //state为0时的动作
+  }else if(state == '1'){
+    //state为1时的动作
+  }
+  while(client.available()) {
+    c = client.read();
+    if (c == '{'){
+      state = client.read();
+    }
+  }
   duration = pulseIn(pin, LOW);
-  lowpulseoccupancy = lowpulseoccupancy + duration;
-  if ((millis() - starttime) > sampletime_ms)
+  lowpulseoccupancy = lowpulseoccupancy+duration;
+
+  if ((millis()-starttime) > sampletime_ms)
   {
-    ratio = lowpulseoccupancy / (sampletime_ms*10.0);  // Integer percentage 0=>100
-    concentration = 1.1*pow(ratio, 3) - 3.8*pow(ratio, 2) + 520 * ratio + 0.62; // using spec sheet curve
+    ratio = lowpulseoccupancy/(sampletime_ms*10.0);  // Integer percentage 0=>100
+    concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; // using spec sheet curve
     pm25val = pm25coef * concentration * 10;
     lowpulseoccupancy = 0;
     starttime = millis();
   }
-  return pm25val;
-}
-//向服务器上传数据
-void updata(float datapoint)
-{
   if (!client.connected() && lastConnected) {
     //Serial.println("disconnecting.");
     client.stop();
   }
-  if (!client.connected() && (millis() - lastConnectionTime > postingInterval)) {
+ 
+  if(!client.connected() && (millis() - lastConnectionTime > postingInterval)) {
     if (client.connect(server, 80)) {
-
+ 
       // send the HTTP PUT request:
       client.print("GET /downup.php?token=arduino&data=");
-      client.print(datapoint);
+      client.print(pm25val);
       client.println(" HTTP/1.1");
       client.println("Host: wxxcc.applinzi.com");
       client.println("User-Agent: arduino-ethernet");
       client.println("Connection: close");
       client.println();
       lastConnectionTime = millis();
-    }
-    else {
+    }else {
       //Serial.println("connection failed");
       //Serial.println("disconnecting.");
       client.stop();
     }
   }
   lastConnected = client.connected();
-}
-//读取开关状态并做出反应
-void switchmode(void)
-{
-  if (state == '0') {
-    //state为0时的动作
-  }
-  else if (state == '1') {
-    //state为1时的动作
-  }
-  while (client.available()) {
-    c = client.read();
-    if (c == '{') {
-      state = client.read();
-    }
-  }
-}
-void setup() {
-  Ethernet.begin(mac);
-  pinMode(pin, INPUT);
-  starttime = millis();
-}
-
-void loop(void) {
-  switchmode();
-  dsm501();
-  updata(pm25val);
-}
+ }
